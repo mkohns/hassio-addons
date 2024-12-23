6abcb085b5478f8406a8bdce3089cc32d78de62c
Kohns, Mirko SF/HZA-ZG3I <kohnsmrk@schaeffler.com>
2024-12-23 09:24:06 +0100

    adjusted settings page in height

--- slideshow/frontend/slideshow/src/pages/config.vue
+++ slideshow/frontend/slideshow/src/pages/config.vue
@@ -1,78 +1,95 @@
 <template>
   <v-container class="d-flex align-center justify-center" style="height: 100vh">
-    <v-card width="80%">
-      <v-card-title
-        ><v-icon color="primary" class="mr-2">mdi-cog</v-icon>Slideshow
-        Settings</v-card-title
-      >
-      <v-card-text class="mt-5">
-        <v-slider
-          v-model="interval"
-          label="Slide Interval"
-          min="1"
-          max="100"
-          step="1"
-          hide-details="true"
-          color="primary"
-        >
-          <template v-slot:append>
-            <v-text-field
-              v-model="interval"
-              label="Seconds"
-              density="compact"
-              style="width: 80px"
-              type="number"
-              variant="outlined"
+    <v-card width="100%">
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <div class="subtitle">
+              <v-icon class="mr-2" color="primary">mdi-cog</v-icon>General
+            </div>
+          </v-col>
+          <v-col>
+            <div class="subtitle">
+              <v-icon class="mr-2" color="primary">mdi-filter</v-icon>Filter
+            </div>
+          </v-col>
+          <v-col>
+            <div class="subtitle">
+              <v-icon class="mr-2" color="primary">mdi-play-box-outline</v-icon
+              >Slide Modes
+            </div>
+          </v-col>
+        </v-row>
+        <v-row class="mt-7">
+          <v-col>
+            <div class="d-flex">
+              <v-text-field
+                class="seconds"
+                v-model="interval"
+                density="compact"
+                label="Seconds"
+                readonly
+                @click="openIntervalDialog"
+                variant="outlined"
+                hide-details
+              ></v-text-field>
+              <v-btn
+                class="icon-size ml-5"
+                @click="timeUp"
+                icon="mdi-plus-circle"
+              >
+              </v-btn>
+              <v-btn
+                class="icon-size ml-1"
+                @click="timeDown"
+                icon="mdi-minus-circle"
+              >
+              </v-btn>
+            </div>
+
+            <v-switch
               hide-details
-            ></v-text-field>
-          </template>
-        </v-slider>
-        <v-switch
-          hide-details
-          color="primary"
-          :label="portraitMode ? 'Portrait Mode' : 'Landscape Mode'"
-          v-model="portraitMode"
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Show Subtitle Overlay"
-          v-model="overlay"
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Show New Indicator"
-          v-model="newchip"
-        >
-        </v-switch>
-        <div class="subtitle">
-          <v-icon class="mr-2" color="primary">mdi-filter</v-icon>Slide Filter
-        </div>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Show Only Favorites"
-          v-model="showOnlyFavorites"
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Show Only Active Images"
-          v-model="showOnlyActive"
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Prioritize New Images"
-          v-model="prioNewImages"
-        ></v-switch>
-        <v-row no-gutters>
-          <v-col cols="6">
+              color="primary"
+              :label="portraitMode ? 'Portrait Mode' : 'Landscape Mode'"
+              v-model="portraitMode"
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Show Subtitle Overlay"
+              v-model="overlay"
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Show New Indicator"
+              v-model="newchip"
+            >
+            </v-switch>
+          </v-col>
+          <v-col>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Show Only Favorites"
+              v-model="showOnlyFavorites"
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Show Only Active Images"
+              v-model="showOnlyActive"
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Prioritize New Images"
+              v-model="prioNewImages"
+            ></v-switch>
             <v-switch
               hide-details
               color="primary"
@@ -80,72 +97,93 @@
               v-model="showOnlyInTimeFrame"
             >
             </v-switch>
-          </v-col>
-          <v-col class="pl-1 pr-1">
             <DateSelect
-              :disabled="!showOnlyInTimeFrame"
+              v-if="showOnlyInTimeFrame"
               label="Start Date"
               v-model="startDate"
               @update:modelValue="startDateUpdate"
             ></DateSelect>
-          </v-col>
-          <v-col class="pl-1 pr-1">
             <DateSelect
               nullValue="Today"
+              v-if="showOnlyInTimeFrame"
               :disabled="!showOnlyInTimeFrame"
               label="End Date"
               v-model="endDate"
               @update:modelValue="endDateUpdate"
             ></DateSelect>
           </v-col>
+          <v-col>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Random (default)"
+              v-model="modeRandom"
+              @update:modelValue="
+                modeChronological = false;
+                modeReverseChronological = false;
+                checkMode();
+              "
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Chronological"
+              v-model="modeChronological"
+              @update:modelValue="
+                modeRandom = false;
+                modeReverseChronological = false;
+                checkMode();
+              "
+            >
+            </v-switch>
+            <v-switch
+              hide-details
+              color="primary"
+              label="Reverse Chronological"
+              v-model="modeReverseChronological"
+              @update:modelValue="
+                modeRandom = false;
+                modeChronological = false;
+                checkMode();
+              "
+            >
+            </v-switch>
+          </v-col>
         </v-row>
 
-        <div class="subtitle">
-          <v-icon class="mr-2" color="primary">mdi-play-box-outline</v-icon
-          >Slide Modes
-        </div>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Random (default)"
-          v-model="modeRandom"
-          @update:modelValue="
-            modeChronological = false;
-            modeReverseChronological = false;
-            checkMode();
-          "
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Chronological"
-          v-model="modeChronological"
-          @update:modelValue="
-            modeRandom = false;
-            modeReverseChronological = false;
-            checkMode();
-          "
-        >
-        </v-switch>
-        <v-switch
-          hide-details
-          color="primary"
-          label="Reverse Chronological"
-          v-model="modeReverseChronological"
-          @update:modelValue="
-            modeRandom = false;
-            modeChronological = false;
-            checkMode();
-          "
-        >
-        </v-switch>
-        <div class="mt-4">
+        <div class="d-flex mt-4">
+          <v-spacer></v-spacer>
           <v-btn color="primary" @click="saveConfig">Save</v-btn>
-          <v-btn class="ml-3" color="primary" @click="goBack">Back</v-btn>
+          <v-btn class="ml-3" color="primary" @click="goBack">Close</v-btn>
         </div>
       </v-card-text>
     </v-card>
+    <v-dialog v-model="intervalDialog" width="70%">
+      <v-card>
+        <div class="dialog-seconds">{{ intervalTmp }} seconds</div>
+        <v-slider
+          v-model="intervalTmp"
+          min="5"
+          max="500"
+          step="10"
+          hide-details="true"
+          color="primary"
+          class="px-10 pb-10"
+        >
+        </v-slider>
+        <div class="d-flex justify-center mb-6">
+          <v-btn
+            class="mr-5"
+            color="primary"
+            text
+            @click="intervalDialog = false"
+            >Close</v-btn
+          >
+          <v-btn color="primary" text @click="setInterval">Apply</v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -154,11 +192,11 @@ import { ref, onMounted, onUnmounted } from "vue";
 import { useSlideshowStore } from "@/stores/slideshow";
 import router from "@/router";
 import DateSelect from "@/components/dateSelect.vue";
-import backend from "@/api/backend";
 
 const store = useSlideshowStore();
 
 const interval = ref(store.interval);
+const intervalTmp = ref(null);
 const overlay = ref(store.showOverlay);
 const newchip = ref(store.showNewChip);
 const showOnlyFavorites = ref(store.showOnlyFavorites);
@@ -171,6 +209,18 @@ const modeReverseChronological = ref(store.modeReverseChronological);
 const startDate = ref(store.startDate);
 const endDate = ref(store.endDate);
 const portraitMode = ref(store.portraitMode);
+
+const intervalDialog = ref(false);
+
+function setInterval() {
+  interval.value = intervalTmp.value;
+  intervalDialog.value = false;
+}
+
+function openIntervalDialog() {
+  intervalTmp.value = interval.value;
+  intervalDialog.value = true;
+}
 
 function startDateUpdate(value) {
   console.log("Start Date Update:", value);
@@ -206,11 +256,23 @@ const saveConfig = () => {
   store.setEndDate(endDate.value);
   //store.setStartDate(null);
   //store.setEndDate(null);
+
+  goBack();
 };
 
 const goBack = () => {
   router.push("/");
 };
+
+function timeUp() {
+  interval.value++;
+}
+
+function timeDown() {
+  if (interval.value > 5) {
+    interval.value--;
+  }
+}
 
 function checkMode() {
   if (
@@ -229,8 +291,20 @@ onUnmounted(() => {});
 
 <style scoped>
 .subtitle {
-  font-size: 1rem;
+  font-size: 1.3rem;
   font-weight: 500;
   margin-top: 1rem;
+}
+.seconds {
+  max-width: 100px;
+}
+.icon-size {
+  font-size: 1.8rem;
+}
+.dialog-seconds {
+  text-align: center;
+  font-size: 3rem;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 </style>
